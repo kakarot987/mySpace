fa6cfaf1ad02b303671adf89e7d9b5e239024953
Mayank Devrani <53875589+kakarot987@users.noreply.github.com>
2021-02-11 10:28:55 +0530

    Add files via upload

---
+++ mySpace/js/mydiary.js
@@ -0,0 +1,15 @@
+function colorGreen(){
+    document.getElementById("inputTextToSave").style.backgroundColor = "lawngreen";
+}
+
+function colorRed(){
+    document.getElementById("inputTextToSave").style.backgroundColor = "#ff6363";
+}
+
+function colorBlue(){
+    document.getElementById("inputTextToSave").style.backgroundColor = "#6edee2";
+}
+
+function colorYellow(){
+    document.getElementById("inputTextToSave").style.backgroundColor = "#f1f888";
+}
